d742a92414df9e5feb62cadbfeaa3a73aff3587e
Zerio <neoboij@gmail.com>
2023-12-28 03:03:24 +0100

    feat: setup player distance checking

--- apps/client/src/init.ts
+++ apps/client/src/init.ts
@@ -1,9 +1,12 @@
 import { VoiceRanges } from "@zerio-voice/utils/data";
-import { Wait } from "@zerio-voice/utils/functions";
+import { getDistance, Wait } from "@zerio-voice/utils/functions";
 import { getConfig } from "@zerio-voice/utils/config";
+import { warn } from "@zerio-voice/utils/logger";
 
 const voiceTarget = 1;
 const serverId = GetPlayerServerId(PlayerId());
+let proximity = MumbleGetTalkerProximity();
+let normalTalkingStatus = false;
 
 onNet("mumbleConnected", async () => {
   MumbleSetTalkerProximity(VoiceRanges.Normal);
@@ -30,33 +33,79 @@ onNet("onPlayerJoining", (id: number) => {
   MumbleAddVoiceChannelListen(id);
 });
 
-let normalTalkingStatus = false;
+function updateNUIVoiceStatus() {
+  const currentTalkingStatus =
+    (MumbleIsPlayerTalking(PlayerId()) as number | boolean) === 1;
+
+  if (currentTalkingStatus !== normalTalkingStatus) {
+    normalTalkingStatus = currentTalkingStatus;
+
+    SendNUIMessage({
+      action: "isTalking",
+
+      data: {
+        normal: normalTalkingStatus,
+      },
+    });
+  }
+}
+
+function proximityCheck(
+  coords1: number[],
+  coords2: number[],
+): [boolean, number] {
+  const dist = getDistance(coords1, coords2);
+
+  return [dist !== -1 && dist < proximity, dist];
+}
+
+function checkForNearbyPlayers() {
+  const plr = PlayerPedId();
+
+  if (plr) {
+    const coords = GetEntityCoords(plr, true);
+    proximity = MumbleGetTalkerProximity();
+
+    MumbleClearVoiceTargetChannels(voiceTarget);
+    MumbleAddVoiceChannelListen(LocalPlayer.state.assignedChannel);
+    MumbleAddVoiceTargetChannel(voiceTarget, LocalPlayer.state.assignedChannel);
+
+    const plrs = GetActivePlayers();
+
+    for (let i = 0; i < plrs.length; i++) {
+      const v = plrs[i];
+      const serverId = GetPlayerServerId(v);
+      const [shouldAdd, _distance] = proximityCheck(
+        coords,
+        GetEntityCoords(GetPlayerPed(v), true),
+      );
+
+      if (shouldAdd) {
+        MumbleAddVoiceTargetChannel(
+          voiceTarget,
+          MumbleGetVoiceChannelFromServerId(serverId),
+        );
+      }
+    }
+  }
+}
+
 onNet("onClientResourceStart", async (resourceName: string) => {
   if (GetCurrentResourceName() == resourceName) {
     const cfg = getConfig();
 
     if (cfg) {
       while (!MumbleIsConnected()) {
-        Wait(250);
+        warn("Awaiting mumble connection");
+        await Wait(250);
       }
 
       setInterval(() => {
         if (cfg.enableUI) {
-          const currentTalkingStatus =
-            (MumbleIsPlayerTalking(PlayerId()) as number | boolean) === 1;
-
-          if (currentTalkingStatus !== normalTalkingStatus) {
-            normalTalkingStatus = currentTalkingStatus;
-
-            SendNUIMessage({
-              action: "isTalking",
-
-              data: {
-                normal: normalTalkingStatus,
-              },
-            });
-          }
+          updateNUIVoiceStatus();
         }
+
+        checkForNearbyPlayers();
       }, 250);
     }
   }
--- packages/utils/src/functions.ts
+++ packages/utils/src/functions.ts
@@ -1,3 +1,24 @@
 export const Wait = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
+
+export function getDistance(coords1: number[], coords2: number[]): number {
+  if (
+    coords1[0] &&
+    coords1[1] &&
+    coords1[2] &&
+    coords2[0] &&
+    coords2[1] &&
+    coords2[2]
+  ) {
+    const dx = coords1[0] - coords2[0];
+    const dy = coords1[1] - coords2[1];
+    const dz = coords1[2] - coords2[2];
+
+    const dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+    return dist;
+  } else {
+    return -1;
+  }
+}
