b39df12ba84c96ed14badab5c8d42909588af478
Zerio <neoboij@gmail.com>
2024-01-03 21:02:04 +0100

    chore: setup linting for readme aswell

---
+++ packages/prettier-config/readme.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  $schema: "https://json.schemastore.org/prettierrc",
+  semi: false,
+  singleQuote: true,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  proseWrap: "always" // printWidth line breaks in md/mdx
+};
